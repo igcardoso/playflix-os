06bb53180df14f9517a2254838e9ec28a1e27d55
igor <85964276+igcardoso@users.noreply.github.com>
2023-07-15 12:00:16 -0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -171,7 +171,8 @@ function updateCarousel(movies, carousel) {
   // Inicializar o Owl Carousel
   carousel.owlCarousel({
     loop: false,
-    margin: 4,
+    margin: 2,
+    focusOnSelect: true, 
     nav: true,
     navText: [`<svg width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="Arrow / Chevron_Left">
