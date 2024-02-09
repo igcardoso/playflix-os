faeba32dc6c03577960a17394aa6fc955df49347
igor <85964276+igcardoso@users.noreply.github.com>
2024-02-09 08:48:03 -0300

    update src/app/javascript/firebase/channels.js

--- src/app/javascript/firebase/channels.js
+++ src/app/javascript/firebase/channels.js
@@ -1,9 +1,15 @@
 let iframe_rendering_channels = document.querySelector('#channels .media iframe');
 
 let channel_1 = document.querySelector('.slide .c1');
+let channel_2 = document.querySelector('.slide .c1');
 
 channel_1.addEventListener('click', ()=> {
   let channel_link = "https://playertv.net/tv.php?c=megapix";
+  
+  iframe_rendering_channels.setAttribute('src', channel_link);
+});
+channel_2.addEventListener('click', ()=> {
+  let channel_link = "https://playertv.net/tv.php?c=tcPremium";
   
   iframe_rendering_channels.setAttribute('src', channel_link);
 });
