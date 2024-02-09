cb03b24127b079098897d17b2d32afea8e8777e9
igor <85964276+igcardoso@users.noreply.github.com>
2024-02-09 16:34:37 -0300

    update src/app/javascript/navigation.js

--- src/app/javascript/navigation.js
+++ src/app/javascript/navigation.js
@@ -58,7 +58,7 @@ function updateHeaderVisibility() {
 		navBar.style.display = "none";
 	} else {
 		navBar.style.display = "flex";
-		header.classList.remove('hidden');
+		// header.classList.remove('hidden');
 		header.classList.remove('hidden-display');
 	}
 
