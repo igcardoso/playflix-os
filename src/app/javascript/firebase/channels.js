40aa664c8d9501366bfe6c543ac985f4ae1a34bc
igor <85964276+igcardoso@users.noreply.github.com>
2024-02-09 09:18:48 -0300

    update src/app/javascript/firebase/channels.js

--- src/app/javascript/firebase/channels.js
+++ src/app/javascript/firebase/channels.js
@@ -2,6 +2,7 @@ let iframe_rendering_channels = document.querySelector('#channels .media iframe'
 
 let channel_1 = document.querySelector('.slide .c1');
 let channel_2 = document.querySelector('.slide .c2');
+let channel_3 = document.querySelector('.slide .c3');
 
 channel_1.addEventListener('click', ()=> {
   let channel_link = "https://playertv.net/tv.php?c=megapix";
@@ -10,6 +11,11 @@ channel_1.addEventListener('click', ()=> {
 });
 channel_2.addEventListener('click', ()=> {
   let channel_link = "https://playertv.net/tv.php?c=tc-premium";
+  
+  iframe_rendering_channels.setAttribute('src', channel_link);
+});
+channel_3.addEventListener('click', ()=> {
+  let channel_link = "https://playertv.net/c/wbr.php?id=tcFun";
   
   iframe_rendering_channels.setAttribute('src', channel_link);
 });
